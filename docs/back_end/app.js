e80f10b3556e872ee0e073b5c4e785cb57244ed1
Sinbad <rob1_93@hotmail.fr>
2021-04-12 17:08:58 +0200

    basic back end ok, trying to sign up in front

--- docs/back_end/app.js
+++ docs/back_end/app.js
@@ -5,12 +5,19 @@ const userRoutes = require('./routes/users');
 const path = require('path');
 
 const app = express();
-mongoose.connect('mongodb+srv://mongodb+srv://Sinbad:MongoDB2021sinbad@perso1712.fat9x.mongodb.net/<myFirstDatabase>?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Sinbad:mhZ2lrmMVgn147S0@perso1712.fat9x.mongodb.net/<openC>?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch((error) => console.log('Connexion à MongoDB échouée !' + error));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
--- docs/back_end/routes/controllers/usersCtrl.js
+++ docs/back_end/routes/controllers/usersCtrl.js
@@ -1,5 +1,6 @@
 const User = require('../../models/usersModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)/* saler le password 10 fois*/
